8c5fc733dabbcb781ef6ab410de1a8f466db3fa6
DavidMcQueen <dave.j.mcqueen@gmail.com>
2015-03-27 22:12:03 +0000

    Added in tests

--- Heart Rate Monitor - AudioTests/Heart_Rate_Monitor___AudioTests.swift
+++ Heart Rate Monitor - AudioTests/Heart_Rate_Monitor___AudioTests.swift
@@ -21,16 +21,176 @@ class Heart_Rate_Monitor___AudioTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock() {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testZoneChanges(){
+        var user = UserHeartRate();
+        
+        var rest = Zone(_lower: nil, _upper: 99, _zone: HeartRateZone.Rest);
+        var zone1 = Zone(_lower: 100, _upper: 119, _zone: HeartRateZone.ZoneOne);
+        let zone2 = Zone(_lower: 120, _upper: 139, _zone: HeartRateZone.ZoneTwo);
+        let zone3 = Zone(_lower: 140, _upper: 159, _zone: HeartRateZone.ZoneThree);
+        let zone4 = Zone(_lower: 160, _upper: 179, _zone: HeartRateZone.ZoneFour);
+        let zone5 = Zone(_lower: 180, _upper: 199, _zone: HeartRateZone.ZoneFive);
+        let max = Zone(_lower: 200, _upper: nil, _zone: HeartRateZone.Max);
+        
+        
+        user.Zones.append(rest);
+        user.Zones.append(zone1);
+        user.Zones.append(zone2);
+        user.Zones.append(zone3);
+        user.Zones.append(zone4);
+        user.Zones.append(zone5);
+        user.Zones.append(max);
+        
+        var currentBPM = 0;
+        
+        currentBPM = 100
+        var newZone = user.getZoneforBPM(currentBPM)
+        XCTAssertTrue(newZone == HeartRateZone.ZoneOne, "Zone match - Pass");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneTwo, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneThree, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFour, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFive, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Max, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Rest, "Zone match - Fail");
+        
+        currentBPM = 119
+        newZone = user.getZoneforBPM(currentBPM)
+        XCTAssertTrue(newZone == HeartRateZone.ZoneOne, "Zone match - Pass");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneTwo, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneThree, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFour, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFive, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Max, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Rest, "Zone match - Fail");
+        
+        currentBPM = 120
+        newZone = user.getZoneforBPM(currentBPM)
+        XCTAssertFalse(newZone == HeartRateZone.ZoneOne, "Zone match - Fail");
+        XCTAssertTrue(newZone == HeartRateZone.ZoneTwo, "Zone match - Pass");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneThree, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFour, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFive, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Max, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Rest, "Zone match - Fail");
+        
+        currentBPM = 139
+        newZone = user.getZoneforBPM(currentBPM)
+        XCTAssertFalse(newZone == HeartRateZone.ZoneOne, "Zone match - Fail");
+        XCTAssertTrue(newZone == HeartRateZone.ZoneTwo, "Zone match - Pass");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneThree, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFour, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFive, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Max, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Rest, "Zone match - Fail");
+        
+        currentBPM = 140
+        newZone = user.getZoneforBPM(currentBPM)
+        XCTAssertFalse(newZone == HeartRateZone.ZoneOne, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneTwo, "Zone match - Fail");
+        XCTAssertTrue(newZone == HeartRateZone.ZoneThree, "Zone match - Pass");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFour, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFive, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Max, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Rest, "Zone match - Fail");
+        
+        currentBPM = 159
+        newZone = user.getZoneforBPM(currentBPM)
+        XCTAssertFalse(newZone == HeartRateZone.ZoneOne, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneTwo, "Zone match - Fail");
+        XCTAssertTrue(newZone == HeartRateZone.ZoneThree, "Zone match - Pass");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFour, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFive, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Max, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Rest, "Zone match - Fail");
+        
+        currentBPM = 160
+        newZone = user.getZoneforBPM(currentBPM)
+        XCTAssertFalse(newZone == HeartRateZone.ZoneOne, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneTwo, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneThree, "Zone match - Fail");
+        XCTAssertTrue(newZone == HeartRateZone.ZoneFour, "Zone match - Pass");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFive, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Max, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Rest, "Zone match - Fail");
+        
+        currentBPM = 179
+        newZone = user.getZoneforBPM(currentBPM)
+        XCTAssertFalse(newZone == HeartRateZone.ZoneOne, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneTwo, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneThree, "Zone match - Fail");
+        XCTAssertTrue(newZone == HeartRateZone.ZoneFour, "Zone match - Pass");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFive, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Max, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Rest, "Zone match - Fail");
+        
+        currentBPM = 180
+        newZone = user.getZoneforBPM(currentBPM)
+        XCTAssertFalse(newZone == HeartRateZone.ZoneOne, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneTwo, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneThree, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFour, "Zone match - Fail");
+        XCTAssertTrue(newZone == HeartRateZone.ZoneFive, "Zone match - Pass");
+        XCTAssertFalse(newZone == HeartRateZone.Max, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Rest, "Zone match - Fail");
+        
+        currentBPM = 199
+        newZone = user.getZoneforBPM(currentBPM)
+        XCTAssertFalse(newZone == HeartRateZone.ZoneOne, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneTwo, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneThree, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFour, "Zone match - Fail");
+        XCTAssertTrue(newZone == HeartRateZone.ZoneFive, "Zone match - Pass");
+        XCTAssertFalse(newZone == HeartRateZone.Max, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Rest, "Zone match - Fail");
+        
+        currentBPM = 200
+        newZone = user.getZoneforBPM(currentBPM)
+        XCTAssertFalse(newZone == HeartRateZone.ZoneOne, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneTwo, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneThree, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFour, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFive, "Zone match - Fail");
+        XCTAssertTrue(newZone == HeartRateZone.Max, "Zone match - Pass");
+        XCTAssertFalse(newZone == HeartRateZone.Rest, "Zone match - Fail");
+        
+        currentBPM = 219
+        newZone = user.getZoneforBPM(currentBPM)
+        XCTAssertFalse(newZone == HeartRateZone.ZoneOne, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneTwo, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneThree, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFour, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFive, "Zone match - Fail");
+        XCTAssertTrue(newZone == HeartRateZone.Max, "Zone match - Pass");
+        XCTAssertFalse(newZone == HeartRateZone.Rest, "Zone match - Fail");
+        
+        currentBPM = 99
+        newZone = user.getZoneforBPM(currentBPM)
+        XCTAssertFalse(newZone == HeartRateZone.ZoneOne, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneTwo, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneThree, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFour, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFive, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Max, "Zone match - Fail");
+        XCTAssertTrue(newZone == HeartRateZone.Rest, "Zone match - Pass");
+        
+        currentBPM = 0
+        newZone = user.getZoneforBPM(currentBPM)
+        XCTAssertFalse(newZone == HeartRateZone.ZoneOne, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneTwo, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneThree, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFour, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.ZoneFive, "Zone match - Fail");
+        XCTAssertFalse(newZone == HeartRateZone.Max, "Zone match - Fail");
+        XCTAssertTrue(newZone == HeartRateZone.Rest, "Zone match - Pass");
+        
+        
     }
     
 }
